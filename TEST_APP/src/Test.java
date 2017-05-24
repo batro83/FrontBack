2186d00897dfaa43fe5a699905da233e0810a0bb
Roger Abad <batro_ska@yahoo.es>
2017-05-24 23:43:52 +0200

    j2ee

---
+++ TEST_APP/src/Test.java
@@ -0,0 +1,10 @@
+import es.back.persistence.test.AppTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AppTest d = new AppTest("nnnnn");
+
+	}
+
+}
---
+++ back-restServices/src/main/java/es/back/restServices/App.java
@@ -0,0 +1,13 @@
+package es.back.restServices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
